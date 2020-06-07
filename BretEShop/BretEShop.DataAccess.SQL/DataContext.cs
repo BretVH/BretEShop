7a39abc47c12dbd409946684d09438fc5b92ce67
Bret A. Van Hof <bretvh@hotmail.com>
2020-06-07 12:06:02 -0400

    Start Code-First EF

---
+++ BretEShop/BretEShop.DataAccess.SQL/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using BretEShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BretEShop.DataAccess.SQL
+{
+    public class DataContext : DbContext
+    {
+        public DataContext()
+            : base("DefaultConnection")
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> productCategories { get; set; }
+    }
+}

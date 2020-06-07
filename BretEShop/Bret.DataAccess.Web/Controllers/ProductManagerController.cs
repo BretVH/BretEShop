8789a1fea093481df911161d3477fee2ae5e98de
Bret A. Van Hof <bretvh@hotmail.com>
2020-06-07 09:02:45 -0400

    ProductManagerController and views

---
+++ BretEShop/Bret.DataAccess.Web/Controllers/ProductManagerController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BretEShop.DataAccess.InMemory;
+using BretEShop.Core;
+using Microsoft.Ajax.Utilities;
+using BretEShop.Core.Models;
+
+namespace Bret.DataAccess.Web.Controllers
+{
+    public class ProductManagerController : Controller
+    {
+        ProductRepository context;
+        public ProductManagerController()
+        {
+            context = new ProductRepository(); 
+        }
+        // GET: ProductManager
+        public ActionResult Index()
+        {
+            List <Product> products = context.GetProducts().ToList();
+            return View(products);
+        }
+
+        public ActionResult Create()
+        {
+            Product product = new Product();
+            return View(product);
+        }
+
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(product);
+            }
+            else
+            {
+                context.Insert(product);
+                context.Commit();
+
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult Edit(string Id)
+        {
+            Product product = context.Find(Id);
+            if(product == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(product);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Product product, string Id)
+        {
+            Product productToEdit = context.Find(Id);
+
+            if(productToEdit == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                if (!ModelState.IsValid)
+                {
+                    return View(product);
+                }
+                productToEdit.Category = product.Category;
+                productToEdit.Description = product.Description;
+                productToEdit.Image = product.Image;
+                productToEdit.Name = product.Name;
+                productToEdit.Price = product.Price;
+
+                context.Commit();
+
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult Delete(string Id)
+        {
+            Product product = context.Find(Id);
+
+            if(product == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(product);
+            }
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult ConfirmDelete(string Id)
+        {
+            Product product = context.Find(Id);
+
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                context.Delete(Id);
+                context.Commit();
+
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}

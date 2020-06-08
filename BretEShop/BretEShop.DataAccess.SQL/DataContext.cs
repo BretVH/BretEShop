5ef695f56864743e94a2e48af3ceb5bbfdeede14
Bret A. Van Hof <bretvh@hotmail.com>
2020-06-08 13:26:41 -0400

    Add Basket/BasketItems

--- BretEShop/BretEShop.DataAccess.SQL/DataContext.cs
+++ BretEShop/BretEShop.DataAccess.SQL/DataContext.cs
@@ -18,5 +18,7 @@ namespace BretEShop.DataAccess.SQL
 
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductCategory> productCategories { get; set; }
+        public DbSet<Basket> Baskets { get; set; }
+        public DbSet<BasketItem> BasketItems { get; set; }
     }
 }

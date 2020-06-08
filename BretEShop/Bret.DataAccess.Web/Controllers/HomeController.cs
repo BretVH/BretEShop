d2392017bca729ba7c243c9277bb1da60f23ff5f
Bret A. Van Hof <bretvh@hotmail.com>
2020-06-08 12:56:41 -0400

    Add filtering by Product category

--- BretEShop/Bret.DataAccess.Web/Controllers/HomeController.cs
+++ BretEShop/Bret.DataAccess.Web/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BretEShop.Core.Contracts;
+using BretEShop.Core.Models;
+using BretEShop.Core.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +11,41 @@ namespace Bret.DataAccess.Web.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+
+        IRepository<Product> context;
+        IRepository<ProductCategory> productCategoriesContext;
+        public HomeController(IRepository<Product> context, IRepository<ProductCategory> productCategoriesContext)
         {
-            return View();
+            this.context = context;
+            this.productCategoriesContext = productCategoriesContext;
+        }
+        public ActionResult Index(string Category = null)
+        {
+            List<Product> products;
+            List<ProductCategory> categories = productCategoriesContext.GetItems().ToList();
+
+            if (Category == null)
+            {
+                products = context.GetItems().ToList();
+            }
+            else
+            {
+                products = context.GetItems().Where(p => p.Category == Category).ToList();
+            }
+
+            ProductListViewModel model = new ProductListViewModel();
+            model.Products = products;
+            model.ProductCategories = categories;
+            
+            return View(model);
+        }
+
+        public ActionResult Details(string Id)
+        {
+            Product product = context.Find(Id);
+            if (product == null)
+                return HttpNotFound();
+            return View(product);
         }
 
         public ActionResult About()
